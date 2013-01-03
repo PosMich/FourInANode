7394ad8ae82d6bdc53c06568b8cbe18b78c7481b
fhs33734 <michael.poschacher@gmail.com>
2013-01-03 14:27:15 +0100

    broadcast/unicast support added first socket.io tests

--- fourinanode.js
+++ fourinanode.js
@@ -1,44 +1,103 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+//  socket.emit('news', { hello: 'world' });
+//  socket.on('my other event', function (data) {
+//    console.log(data);
+//  });
+  socket.on('set player', function( data ) {
+    CLIENTNAME = data;
+    debug( console.log("new player name: "+CLIENTNAME) );
+    socket.emit('new name', CLIENTNAME);
   });
+  socket.on('start game with', function( data ) {
+    UNICAST = data;
+    debug( console.log("Play with: " + UNICAST) );
+  })
 });
- 
-var VERSION = 1;
-var PORT = 32442;
-var CLIENTTYPE = 1;
-var STAGE = 0;
-var CLIENTNAME = "";
 
-var dgram = require("dgram"); 
-dgram.setBroadcast(true);
-dgram.setMulticastTTL(255);
-dgram.setMulticastLoopback(true);
-var server = dgram.createSocket("udp4");
 
-var messages = {
-  search: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:1,clientname:CLIENTNAME}),
-  found: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:2,clientname:CLIENTNAME}),
-  accepted: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:2}),
-  ready: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:3}),
-  turn: function (col) { 
-    return JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:4,column:col})
-  },
-  end: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:5}),
-  abort: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE, stage:99})
+var DEBUG = 1;
+
+
+/*********** DEBUG HELPER *******************/
+function debug( doThis ) {
+  if (DEBUG === 1)
+    eval("(function() { "+doThis+"})()");
 }
 
-var message = new Buffer( messages.search );
+
+/*********************** GAME OPTIONS *****************************/
+var PORT      = 32442;                  // working port
+var BROADCAST = "255.255.255.255";      // BROADCAST address
+var UNICAST;                            // Unicast address
+var TARGET    = BROADCAST;              // target address
+var INTERVAL;                           // Interval ID 
 
 
-server.send(message, 0, message.length, PORT, rinfo.address, function(err, bytes) {
-if (err) throw err;
-console.log("msg " + message + " sent to " + rinfo.address + ":" + rinfo.port);
-}); 
-
-
+/*********************** UDP VARS *********************************/
+var dgram  = require("dgram");
+var server = dgram.createSocket("udp4");
 
 server.bind(PORT);
+debug(server.setMulticastLoopback(true));
+
+
+/*********************** JSON MESSAGE VARS ************************/ 
+var VERSION    = 2;
+var CLIENTTYPE = 1;
+var STAGE      = 0;
+var CLIENTNAME = "Player";
+var TURN       = 0;
+var COLUMN     = 0;
+var CURR_MSG   = new Buffer ( "" );
+
+var messages = {
+  search:   JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:1,clientname:CLIENTNAME}),
+  found:    JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:2,clientname:CLIENTNAME}),
+  accepted: JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:2}),
+  ready:    JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:3}),
+  turn:     JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:4,column:COLUMN, turn:TURN}),
+  end:      JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:5}),
+  abort:    JSON.stringify({version:VERSION,clienttype:CLIENTTYPE,stage:99})
+}
+
+function send() {
+  server.send( CURR_MSG, 0, CURR_MSG.length, PORT, TARGET, function( err, bytes ) {
+    if (err) throw err;
+    debug(console.log("msg " + CURR_MSG + " sent to " + TARGET + ":" + PORT));
+  })
+}
+
+function enableBroadcast() {
+  server.setBroadcast(true);
+  TARGET = BROADCAST;
+}
+
+function enableUnicast( IP ) {
+  server.setBroadcast(false);
+  TARGET = UNICAST = IP;
+}
+
+function updateMessage( message ) {
+  endTransmission();
+  
+  if ( typeof( message ) !== "undefined" ) {
+    CURR_MSG = new Buffer( message );
+  }
+  
+  send();
+  INTERVAL = setInterval( send, 2000);
+}
+
+function endTransmission() {
+  clearInterval( INTERVAL );
+}
+
+
+// to start the whole thing 
+//enableBroadcast();
+//updateMessage( messages.search );
+
+// test if update and unicast works
+//setTimeout(function() { enableUnicast( "10.0.0.138" ); updateMessage( messages.end ); }, 7000);
